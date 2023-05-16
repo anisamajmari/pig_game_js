83b1643c91b0d7092a81995785dd99ddd5d03a0d
Anisa Majmari <anisamajmari@gmail.com>
2023-05-16 14:40:34 +0200

    starting

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-'use strict';
+"use strict";
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+
+const diceEl = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const btnNew = document.querySelector(".btn--new");
+
+let scores, currentScore, playing, activePlayer;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+};
+init();
