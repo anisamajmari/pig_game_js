187d115468a9892b99730cb2844e4461a10bd3fb
Anisa Majmari <anisamajmari@gmail.com>
2023-05-16 14:56:35 +0200

    rolling dice

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ const init = function () {
   player1El.classList.remove("player--winner");
 };
 init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+//Rolling dice
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove("hidden");
+    diceEl.src = `img/dice-${dice}.png`;
+
+    //Check for rolled 1
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
