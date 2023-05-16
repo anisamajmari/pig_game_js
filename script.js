9c73a8cbd1fbe69c9c9576874b85cf52198a9fc9
Anisa Majmari <anisamajmari@gmail.com>
2023-05-16 15:04:39 +0200

    hold scores

--- script.js
+++ script.js
@@ -57,3 +57,29 @@ btnRoll.addEventListener("click", function () {
     }
   }
 });
+
+//Hold scores
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+//New game
+btnNew.addEventListener("click", init);
